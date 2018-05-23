cfe6ac453d89e3ef40c24732281f4e37bdb528f7
demi <idd90i@gmail.com>
2018-05-23 07:08:37 +0300

    problem with network in docker

--- docker/start.sh
+++ docker/start.sh
@@ -4,17 +4,19 @@ cd ../ && mvn clean package
 cp ./target/ROOT.war ./docker/ROOT.war
 cd ./docker
 
-docker build -t lamoda-test:1.0
+docker build -t lamoda-test:1.0 .
 
-docker run --name my-postgres -v /$(pwd)/_tmp_data:/var/lib/postgresql/data \
+docker run --add-host postgres-host:192.168.5.1 --name my-postgres -v /$(pwd)/_tmp_data:/var/lib/postgresql/data \
 	-e POSTGRES_PASSWORD=p@ss \
 	-e POSTGRES_USER=postgres \
 	-d postgres:9.6
 
-docker run --name lamoda-test-app --link my-postgres:postgres -d \
-	-p 8080:8080 \
+sleep 2
+
+docker run --name lamoda-test-app --link my-postgres:postgres \
+    -p 8080:8080 \
 	-e POSTGRES_PASSWORD=p@ss \
-	-e POSTGRES_URL=jdbc:postgresql://localhost:5432/lamodatest \
+	-e POSTGRES_URL=jdbc:postgresql://DB_ALIAS/postgres \
 	-e POSTGRES_USER=postgres \
 	lamoda-test:1.0
 
